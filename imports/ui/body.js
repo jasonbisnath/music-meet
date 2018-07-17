bbb76871dc536cffba29d8b0b4af000e4563a220
Jason Bisnath <jasonbisnath@Jasons-MacBook-Pro.local>
2018-07-17 16:55:04 -0400

    added buttons and style stuff

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,23 +1,54 @@
 import { Template } from 'meteor/templating';
  
 import './body.html';
- 
-Template.body.helpers({
-  genres: [
-    { text: 'Hip Hop/Rap'},
-    { text: 'Pop' },
-    { text: 'Country' },
-    { text: 'Rock' },
-    { text: 'Jazz' },
-    { text: 'Classical' }
-  ],
-});
+
+var show = "everything";
+window.onload = function() {
+	document.querySelector('.hiphop-button').addEventListener('click', function() {
+		if (show === "everything") {
+			document.querySelector('.Pop').style.display = "none";
+			document.querySelector('.Country').style.display = "none";
+			document.querySelector('.Rock').style.display = "none";
+			document.querySelector('.Classical').style.display = "none";
+			document.querySelector('.Jazz').style.display = "none";
+			show = "hiphop";
+		} else {
+			console.log('else');
+			document.querySelector('.Pop').style.display = "inline";
+			document.querySelector('.Country').style.display = "inline";
+			document.querySelector('.Rock').style.display = "inline";
+			document.querySelector('.Classical').style.display = "inline";
+			document.querySelector('.Jazz').style.display = "inline";
+			show = "everything";
+		}
+	});	
+	document.querySelector('.pop-button').addEventListener('click', function() {
+		if (show === "everything") {
+			document.querySelector('.Rap').style.display = "none";
+			document.querySelector('.Country').style.display = "none";
+			document.querySelector('.Rock').style.display = "none";
+			document.querySelector('.Classical').style.display = "none";
+			document.querySelector('.Jazz').style.display = "none";
+			show = "pop";
+		} else {
+			console.log('else');
+			document.querySelector('.Rap').style.display = "inline";
+			document.querySelector('.Country').style.display = "inline";
+			document.querySelector('.Rock').style.display = "inline";
+			document.querySelector('.Classical').style.display = "inline";
+			document.querySelector('.Jazz').style.display = "inline";
+			show = "everything";
+		}	
+	});
+};
 
 Template.body.helpers({
   users: [
-    { name: 'JOE', genre: 'Pop', instrument: 'Guitar'},
+    { name: 'JOE', genre: 'Pop', instrument: 'Guitar', custom: "blah"},
     { name: 'BOB', genre: 'Country' },
-    { name: 'IDK', genre: 'Hip Hop/Rap'},
+    { name: 'IDK', genre: 'Rap'},
     { name: 'IDKK', genre: 'Classical'},
-  ],
+    { name: 'veronica', genre: 'Rock'},
+    { name: 'Vithya', genre: 'Jazz'}
+  ]
 });
