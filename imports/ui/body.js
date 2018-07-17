652b3160ef95e3aa9a9baf5879b03abc99639e3e
Jason Bisnath <jasonbisnath@Jasons-MacBook-Pro.local>
2018-07-17 14:20:59 -0400

    changes

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -3,11 +3,21 @@ import { Template } from 'meteor/templating';
 import './body.html';
  
 Template.body.helpers({
-  tasks: [
-    { text: 'Hip Hop/Rap' },
+  genres: [
+    { text: 'Hip Hop/Rap'},
     { text: 'Pop' },
     { text: 'Country' },
     { text: 'Rock' },
+    { text: 'Jazz' },
+    { text: 'Classical' }
   ],
 });
 
+Template.body.helpers({
+  users: [
+    { name: 'JOE', genre: 'Pop', instrument: 'Guitar'},
+    { name: 'BOB', genre: 'Country' },
+    { name: 'IDK', genre: 'Hip Hop/Rap'},
+    { name: 'IDKK', genre: 'Classical'},
+  ],
+});
