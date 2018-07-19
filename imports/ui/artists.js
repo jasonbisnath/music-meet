17a57c88b12d9c8ce04d2e30c6ca58db98aafd5f
Jason Bisnath <jasonbisnath@Jasons-MacBook-Pro.local>
2018-07-19 16:54:51 -0400

    final changes for thursday

---
+++ imports/ui/artists.js
@@ -0,0 +1,17 @@
+// import { Template } from 'meteor/templating';
+ 
+// import { Artists } from '../api/artists.js';
+ 
+// import './artists.html';
+ 
+// Template.user.events({
+//   'click .toggle-checked'() {
+//     // Set the checked property to the opposite of its current value
+//     Artists.update(this._id, {
+//       $set: { checked: ! this.checked },
+//     });
+//   },
+//   'click .delete'() {
+//     Artists.remove(this._id);
+//   },
+// });
--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,38 +1,48 @@
 import { Template } from 'meteor/templating';
  
-import { Users } from '../api/users.js';
+import { Artists } from '../api/artists.js';
  
 import './body.html';
 
+import { Accounts } from 'meteor/accounts-base';
 
+// import './artists.js';
+
+ 
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_ONLY',
+});
+
+var isShowing = true
 function toggleVisible(filter, show, template) {
 	var array = ["Rap", "Pop", "Country", "Rock", "Jazz", "Classical"];
-	for (var genre of array) {
-		if (genre !== filter) {
-			if (show) {
-				for (var thing of template.findAll('.' + genre)) {
-					thing.style.display = "inline";
-				}
-			} else {
-				for (var thing of template.findAll('.' + genre)) {
-					thing.style.display = "none";
-				}
-			}
+	var divsToChange = [];
+	for (var userContainer of template.findAll('.user-container')) {
+		var classes = userContainer.className.split(" ");
+		if (classes.indexOf(filter) === -1) {
+			divsToChange.push(userContainer);
 		}
 	}
+
+	var style = isShowing ? "none" : "inline";
+
+	for (var divs of divsToChange) {
+		divs.style.display = style;
+	}
+	isShowing = !isShowing;
 }
 
  
 Template.body.helpers({
-  users() {
-    return Users.find({});
+  artists() {
+    return Artists.find({});
   }
 });
 
 Template.body.events({
 	'submit .new-user'(event, template) {
 		event.preventDefault();
-		Users.insert({
+		Artists.insert({
 			name: template.find('.name').value,
 			contact: template.find('.contact').value,
 			rap: template.find('.rap').checked,
@@ -48,7 +58,37 @@ Template.body.events({
 
 	'click .rap-button'(event, template) {
 		event.preventDefault();
-		console.log('hi');
-		toggleVisible("Rap", false, template);
+		toggleVisible("Rap", isShowing, template);
+		
 	},
+
+	'click .pop-button'(event, template) {
+		event.preventDefault();
+		toggleVisible("Pop", isShowing, template);
+		
+	},
+
+	'click .country-button'(event, template) {
+		event.preventDefault();
+		toggleVisible("Country", isShowing, template);
+		
+	},
+
+	'click .rock-button'(event, template) {
+		event.preventDefault();
+		toggleVisible("Rock", isShowing, template);
+		
+	},
+
+	'click .jazz-button'(event, template) {
+		event.preventDefault();
+		toggleVisible("Jazz", isShowing, template);
+		
+	},
+
+	'click .classical-button'(event, template) {
+		event.preventDefault();
+		toggleVisible("Classical", isShowing, template);
+		
+	}
 });
